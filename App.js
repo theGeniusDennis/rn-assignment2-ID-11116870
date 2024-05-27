46d4f0f7945a64d8f59c28c051bc32a034372b90
theGeniusDennis <danim005@st.ug.edu.gh>
2024-05-27 13:16:09 -0700

    update

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+      My name is <Text style={styles.boldText}>Dennis Anim.</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#236745',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold'
+  }
+});
